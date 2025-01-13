d18b9046060e309df55a7e5b0a89f245f0c1d414
SefatNur <18029450@uhi.ac.uk>
2025-01-13 18:08:13 +0000

    API key updated

--- src/RestUtils/GetFromGemini.js
+++ src/RestUtils/GetFromGemini.js
@@ -4,7 +4,7 @@ export async function handleUpload(prompt, schema) {
     try {
         // Generate content using Google Generative AI
         // It is recommended to use the API key as an environment variable
-        const genAI = new GoogleGenerativeAI("YOUR_Gemini_KEY");
+        const genAI = new GoogleGenerativeAI("AIzaSyDzEGjWICyC373PK5lXbd6g_83B8fNACBQ");
 
         const model = genAI.getGenerativeModel({
             model: "gemini-1.5-pro",
